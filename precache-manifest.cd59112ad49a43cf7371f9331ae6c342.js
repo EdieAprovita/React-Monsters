fc4d2e4c2c9c7d8443335612afb9f7a5d8fcba97
EdieAprovita <edieveg316@gmail.com>
2020-05-07 20:41:04 -0500

    Updates

---
+++ precache-manifest.cd59112ad49a43cf7371f9331ae6c342.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a4d213dd0e8eac82126a7b0133a2f0",
+    "url": "/React-Monsters/index.html"
+  },
+  {
+    "revision": "696d418c5b3c73762d14",
+    "url": "/React-Monsters/static/css/main.44d55b71.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/React-Monsters/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Monsters/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "696d418c5b3c73762d14",
+    "url": "/React-Monsters/static/js/main.adadeaaf.chunk.js"
+  },
+  {
+    "revision": "915f7bab85f55cdf48c0",
+    "url": "/React-Monsters/static/js/runtime-main.95c16f18.js"
+  }
+]);
